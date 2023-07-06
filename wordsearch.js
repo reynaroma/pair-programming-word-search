8ad58e22dedc5aaba592993f4826cd6156684984
Reyna May <roma.reynamayA@gmail.com>
2023-07-05 21:11:54 -0600

    Add return and another var and loop for the vertical search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,25 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    
+    // Check horizontally
+    for (const l of horizontalJoin) {
+      if (l.includes(word)) {
+        return true;
+      }
     }
-}
+    
+    // Transpose the letters array to switch rows and columns
+    const transposedLetters = letters[0].map((_, colIndex) => letters.map(row => row[colIndex]));
+    const verticalJoin = transposedLetters.map(ls => ls.join(''));
+    
+    // Check vertically
+    for (const l of verticalJoin) {
+      if (l.includes(word)) {
+        return true;
+      }
+    }
+    
+    return false;
+  };
 
 module.exports = wordSearch
